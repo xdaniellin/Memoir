041323b983909483d87e3423814326f57914d657
Nicholas <nicnaude@gmail.com>
2016-12-03 17:05:17 -0800

    Implements icon and kills broken segues. Adds dummy images for UI mock ups.

---
+++ samples/VideoPlayback-6-0-120/VideoPlayback/UIColorExtension.swift
@@ -0,0 +1,21 @@
+//
+//  UIColorExtension.swift
+//  memoire_codepathProject
+//
+//  Created by Nicholas Naudé on 01/12/2016.
+//  Copyright © 2016 Samihah Azim. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static func memoirBlue() -> UIColor {
+        return UIColor(red:0.21, green:0.74, blue:0.74, alpha:1.0)
+        //OLD
+        //return UIColor(red:0.40, green:0.76, blue:0.75, alpha:1.0)
+    }
+    static func memoirBlack() -> UIColor {
+        return UIColor(red:0.10, green:0.11, blue:0.11, alpha:1.0)
+    }
+}
--- samples/VideoPlayback-6-0-120/VideoPlayback/menuViewController.swift
+++ samples/VideoPlayback-6-0-120/VideoPlayback/menuViewController.swift
@@ -12,27 +12,14 @@ class menuViewController: UIViewController {
     @IBAction func didTapCloseButton(_ sender: AnyObject) {
         //dismiss(animated: true)
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func onXButtonTapped(_ sender: Any) {
+       dismiss(animated: true, completion: nil)
     }
-    */
-
 }
