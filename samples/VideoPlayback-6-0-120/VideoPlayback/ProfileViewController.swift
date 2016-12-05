1e05b7ada26b4b5f457b70abf3519fbaeb48af9b
Nicholas <nicnaude@gmail.com>
2016-12-05 15:37:59 -0800

    Implements Profile viewcontroller.

--- samples/VideoPlayback-6-0-120/VideoPlayback/ProfileViewController.swift
+++ samples/VideoPlayback-6-0-120/VideoPlayback/ProfileViewController.swift
@@ -9,17 +9,71 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
-
+    
+    @IBOutlet var backgroundView: UIScrollView!
+    @IBOutlet var scrollView: UIScrollView!
+    @IBOutlet weak var account: UIView!
+    @IBOutlet weak var address: UIView!
+    @IBOutlet weak var creditcard: UIView!
+    @IBOutlet weak var topAssets: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        scrollView.contentSize = CGSize(width: 375, height: 1100)
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        //top assets
+        topAssets.alpha = 0.0
+        
+        UIView.animate(withDuration: 1.0, delay: 0.7,
+                       options: [],
+                       animations: {
+                        self.topAssets.alpha = 1.0
+        }, completion: nil)
+        
+        
+        //account
+        account.alpha = 0.0
+        
+        UIView.animate(withDuration: 1.0, delay: 0.7,
+                       options: [],
+                       animations: {
+                        self.account.alpha = 1.0
+        }, completion: nil)
+        
+        //address
+        address.alpha = 0.0
+        
+        UIView.animate(withDuration: 1.0, delay: 0.7,
+                       options: [],
+                       animations: {
+                        self.address.alpha = 1.0
+        }, completion: nil)
+        
+        //creditcard
+        creditcard.alpha = 0.0
+        
+        UIView.animate(withDuration: 1.0, delay: 0.7,
+                       options: [],
+                       animations: {
+                        self.creditcard.alpha = 1.0
+        }, completion: nil)
+    }
+    
+    
     override func viewDidLayoutSubviews() {
-        // Setup common UI elemennts:
-        //setUpUI()
+        backgroundView.backgroundColor = UIColor.memoirBlack()
+        setUpUI()
     }
-
-
+    @IBAction func onDismissTapped(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func onSaveButtonTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
 }
